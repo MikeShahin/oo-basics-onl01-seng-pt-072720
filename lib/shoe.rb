21f1f3cf9e8e64e821862c7afe17712f77b70db0
Mike Shahindoust <gobias101@gmail.com>
2020-08-13 22:26:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,11 @@
 # Make your shoe class here!
 
 class Shoe
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  #attr_accessor :author, :page_count, :genre
+  attr_reader :brand
  
-  def initialize(title)
-    @title = title
-  end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def initialize(brand)
+    @brand = brand
   end
  
 end
